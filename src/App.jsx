b1daeb55ada5a42701a6559b1e399268a59170f4
Evgen <maystrov1987@gmail.com>
2023-05-04 20:16:57 +0300

    Added title

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-import { useState } from 'react';
-import './index.scss';
+import { useState } from "react";
+import "./index.scss";
 
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: [
-      'приложение',
-      'часть приложения или страницы',
-      'то, что я не знаю что такое',
-    ],
+    title: "Компонент - это ... ",
+    variants: ["приложение", "часть приложения или страницы", "то, что я не знаю что такое"],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
+    title: "Что такое JSX?",
     variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но c возможностью выполнять JS-код',
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но c возможностью выполнять JS-код",
     ],
     correct: 2,
   },
@@ -30,14 +26,9 @@ const questions = [
 function Result({ correct }) {
   return (
     <div className="result">
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
-        alt="result"
-      />
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="result" />
       <h2>Вы отгадали {correct} ответа из 10</h2>
-      <button onClick={() => window.location.reload()}>
-        Попробовать снова
-      </button>
+      <button onClick={() => window.location.reload()}>Попробовать снова</button>
     </div>
   );
 }
@@ -48,11 +39,9 @@ function Game({ step, onVariantClick }) {
   const progress = Math.floor((step / questions.length) * 100);
   return (
     <>
+      <h1 style={{ marginBottom: 30, textAlign: "center" }}>Quizz game</h1>
       <div className="progress">
-        <div
-          style={{ width: `${progress}%` }}
-          className="progress__inner"
-        ></div>
+        <div style={{ width: `${progress}%` }} className="progress__inner"></div>
       </div>
       <h1>{title}</h1>
       <ul>
